5f9614c9327e77a09b4340858b7ec362fa0358d9
Aditya Goyal <141710136+AdityaGoyal1505@users.noreply.github.com>
2025-07-16 12:29:31 +0530

    Update StudentProfileRepository.java

--- src/main/java/com/example/spring_first_project/repository/StudentProfileRepository.java
+++ src/main/java/com/example/spring_first_project/repository/StudentProfileRepository.java
@@ -3,6 +3,7 @@ package com.example.spring_first_project.repository;
 import com.example.spring_first_project.Model.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 import java.util.List;
 
