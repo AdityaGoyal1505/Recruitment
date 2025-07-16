4bab3ce204e20472426ac8adcd26b4710e893fc7
Aditya Goyal <141710136+AdityaGoyal1505@users.noreply.github.com>
2025-07-16 11:51:47 +0530

    Update ApplicationRequest.java

--- src/main/java/com/example/spring_first_project/dto/ApplicationRequest.java
+++ src/main/java/com/example/spring_first_project/dto/ApplicationRequest.java
@@ -1,3 +1,6 @@
+package com.example.spring_first_project.dto;
+import lombok.*;
+@Data
 public class ApplicationRequest {
     private Long userId;  // ID of the logged-in user
     private Long jobId;
