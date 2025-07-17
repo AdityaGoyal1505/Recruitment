41c9f9dd74dc14d94af34954b40b5e5a1ebe5c3c
Aditya Goyal <141710136+AdityaGoyal1505@users.noreply.github.com>
2025-07-17 16:11:16 +0530

    Update RecruiterProfileRepository.java

--- src/main/java/com/example/spring_first_project/repository/RecruiterProfileRepository.java
+++ src/main/java/com/example/spring_first_project/repository/RecruiterProfileRepository.java
@@ -2,11 +2,11 @@ package com.example.spring_first_project.repository;
 import com.example.spring_first_project.Model.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+import java.util.Optional;
 import java.util.List;
 
 @Repository
 public interface RecruiterProfileRepository extends JpaRepository<RecruiterProfile, Long> {
-    RecruiterProfile findByUserId(Long userId);
+    Optional<RecruiterProfile> findByUserId(Long userId);
 }
 
